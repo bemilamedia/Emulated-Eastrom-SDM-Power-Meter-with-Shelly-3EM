db6a59cbc30aa7136fb40902db4bd8056155d213
Dominic <92870821+bemilamedia@users.noreply.github.com>
2022-10-06 13:34:33 +0200

    Update private-h.sample.h

--- private-h.sample.h
+++ private-h.sample.h
@@ -7,4 +7,8 @@ const char* mqtt_server = "192.168.0.10";                           // IP of the
 const char* mqtt_username = "user";                                 // MQTT username
 const char* mqtt_password = "password";                             // MQTT password
 const char* clientID = "fakesdm";                                   // MQTT client ID
-const char* mqtt_topic = "shellies/shellyem-house/emeter/0/power";  // MQTT topic to get power reading from
+//const char* mqtt_topic = "shellies/Saldierung_Zaehler/Power";  // MQTT topic to get power calculated balance from the shellie 3EM over Homeassistant
+// Setting the 3 MQTT topics to your shelly 3EM
+const char* mqtt_topic_phase1 = "shellies/shellyem3-smartmeter/emeter/0/power";  // MQTT topic to get power reading from Phase 1
+const char* mqtt_topic_phase2 = "shellies/shellyem3-smartmeter/emeter/1/power";  // MQTT topic to get power reading from Phase 2
+const char* mqtt_topic_phase3 = "shellies/shellyem3-smartmeter/emeter/2/power";  // MQTT topic to get power reading from Phase 3
